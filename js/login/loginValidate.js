673aff9b78d856dfad47a4ae513513d78970414f
Dante <ajsantos.082@gmail.com>
2024-06-11 12:54:58 +0200

    added pictures to proyect

--- js/login/loginValidate.js
+++ js/login/loginValidate.js
@@ -7,14 +7,8 @@ const pass = urlParams.get("pass");
 console.log(user)
 console.log(pass)
 
-
 if (user == 'user' && pass == 'pass') {
-    // window.open('../html/dashboard.html')
     window.location.href = '../html/dashboard.html'
-
-    
 } else {
-    // window.open('../index.html')
     window.location.href = '../index.html'
-
 }
