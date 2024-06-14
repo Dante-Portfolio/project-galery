e3f6ed226d19d6fab02de0410e49efa4ba9feaaa
Dante <ajsantos.082@gmail.com>
2024-06-14 05:43:21 +0200

    feat - dashboard css & effects. new images webp

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,6 +1,17 @@
 const authParams = new URLSearchParams(window.location.search);
 
-if (!authParams.get('auth')) {
-    alert('Logueate Primero')
-    window.location.href = '../index.html'
-}
+// if (!authParams.get('auth')) {
+//     alert('Logueate Primero')
+//     window.location.href = '../index.html'
+// }
+
+const pics = document.getElementsByClassName('img')
+console.log(pics)
+let contador = 0
+
+for (let pic of pics) {
+    console.log(pic)
+    contador += 1
+    // pic.textContent = contador
+    pic.style.backgroundImage = 'url(../resources/pictures/' + contador + '.webp'
+}  
